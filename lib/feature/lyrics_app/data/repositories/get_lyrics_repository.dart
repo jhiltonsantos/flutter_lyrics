c3ddf199f8a1e1747999060b518bfc92add7710b
jhiltonsantos <jhilton930@gmail.com>
2022-05-31 00:32:24 -0300

    update repositories

--- lib/feature/lyrics_app/data/repositories/get_lyrics_repository.dart
+++ lib/feature/lyrics_app/data/repositories/get_lyrics_repository.dart
@@ -4,6 +4,7 @@ import 'package:flutter_lyrics/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_lyrics/feature/lyrics_app/domain/repositories/i_get_lyrics_repository.dart';
 
+// http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=32016690&apikey=77433270cf4522cb6e2c8cbc82d510f7
 class GetLyricsRepository implements IGetLyricsRepository {
   @override
   Uri urlController = Uri.parse(AppConstants.API_KEY);
